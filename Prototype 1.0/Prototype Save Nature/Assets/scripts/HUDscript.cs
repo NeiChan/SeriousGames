3ff8c7cf3b24e3d603763d8812fa1671bf6af23e
Vorzious <tdvwit@gmail.com>
2016-05-09 16:53:18 +0200

    Prototype 1
    First prototype.

---
+++ Prototype 1.0/Prototype Save Nature/Assets/scripts/HUDscript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDscript : MonoBehaviour {
+
+	float playerScore = 0;
+
+	// Update is called once per frame
+	void Update () {
+		playerScore += Time.deltaTime;
+	}
+
+	public void increaseScore(int amount)
+	{
+		playerScore += amount;
+	}
+
+	void OnDisable()
+	{
+		PlayerPrefs.SetInt ("Score", (int)(playerScore * 100));
+	}
+
+	void OnGUI()
+	{
+		GUI.contentColor = Color.black;
+		GUI.Label (new Rect (10, 10, 100, 30), "Score: " + (int) (playerScore * 100));
+	}
+}
